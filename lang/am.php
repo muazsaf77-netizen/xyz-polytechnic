f6cebdf73ad848edd07607343df361eaef3f0866
muazsaf77-netizen <muazsaf77@gmail.com>
2026-06-15 19:59:25 +0300

    Add: Moodle API Service and language translation files (English + Amharic)

---
+++ lang/am.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+// Language strings (Amharic - አማርኛ)
+return [
+    // Navigation
+    'nav_home'          => 'ቤት',
+    'nav_about'         => 'ስለ እኛ',
+    'nav_academics'     => 'መርሃግብሮች',
+    'nav_admissions'    => 'ምዝገባ',
+    'nav_news'          => 'ዜናዎች',
+    'nav_industry'      => 'ቦታ ላይ ሥልጠና',
+    'nav_student_login' => 'የተማሪ መግቢያ',
+
+    // Hero/CTA
+    'hero_cta'          => 'አሁን ያመልክቱ',
+
+    // Forms
+    'username_label'    => 'ተጠቃሚ ስም',
+    'password_label'    => 'ይለፍ ሃረግ',
+    'btn_login'         => 'ግባ',
+    'btn_submit'        => 'ያስረክቡ',
+    'btn_cancel'        => 'ይወቁ',
+    'btn_save'          => 'ያስቀምጡ',
+    'btn_edit'          => 'ያርም',
+    'btn_delete'        => 'ያጥፉ',
+
+    // Messages
+    'student_login'     => 'የተማሪ ፖርታል መግቢያ',
+    'login_success'     => 'መግቢያ ስኬታማ ነበር! እንኳን ደህና ወደ ምትክህ መለስ።',
+    'login_failed'      => 'ልክ ያልሆነ ተጠቃሚ ስም ወይም ይለፍ ሃረግ።',
+    'session_expired'   => 'የወቅተ ስብሰባዎ ተለወጠ። እባክዎ ዳግም ግቡ።',
+
+    // Validation
+    'validation_required' => 'ይህ መስክ ያስፈልጋል።',
+    'validation_email'    => 'እባክዎ ትክክለኛ ኢሜይል አድራሻ ያስገቡ።',
+    'validation_phone'    => 'እባክዎ ትክክለኛ ስልክ ቁጥር ያስገቡ።',
+
+    // Admin
+    'admin_dashboard'   => 'ዳሽቦርድ',
+    'admin_programs'    => 'መርሃግብሮች',
+    'admin_courses'     => 'ትምህርቶች',
+    'admin_trainers'    => 'አሰልጣኞች',
+    'admin_students'    => 'ተማሪዎች',
+    'admin_news'        => 'ዜናዎች',
+    'admin_coc'         => 'CoC ውጤቶች',
+    'admin_admissions'  => 'ምዝገባዎች',
+    'admin_settings'    => '셋팅',
+];
